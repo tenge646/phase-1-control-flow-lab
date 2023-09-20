ddf9385f8d566f9384c1b0ed216bae04c176334c
kinsman646 <teesam2004@gmail.com>
2023-09-21 00:17:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+
+if (feet <= 400) {
+  
+  return 'This one is on me!';
+} else if (feet > 2000 && feet <= 2500) {
+  
+  return 'I will gladly take your thirty bucks.';
+} 
+ 
+if (feet > 2500) {
+  
+  return 'No can do.';
+} else {
+  
+  return 'Invalid input';
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+
+function ternaryCheckCity(city) {
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  }
+  else {
+    return 'No go.'
+  }
+  
+}
+  
+ 
+
+
+function switchOnCharmFromTip(tip) {
+  if (tip === "generous") {
+    return "Thank you so much.";
+  } else if (tip === "not as generous") {
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
